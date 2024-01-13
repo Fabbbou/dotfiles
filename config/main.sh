412383b02c2716a2826704a32bf7af2e65266f45
Fabbbou <18026601+Fabbbou@users.noreply.github.com>
2024-01-13 01:42:05 +0100

    fix local.sh location in main.sh

--- config/main.sh
+++ config/main.sh
@@ -5,7 +5,7 @@ export DOTFILES_HOME="$HOME/.dotfiles"
 # Add your sh files you want to use to source your bash such as alias, or variables here
 ########################
 source "$DOTFILES_HOME/config/alias.sh"
-source "$DOTFILES_HOME/config/local.sh"
+source "$DOTFILES_HOME/local.sh"
 
 #######################
 # Add in PATH 
