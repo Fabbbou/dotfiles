45022cff59dac29698124361f38b53001c0fd779
Fabbbou <18026601+Fabbbou@users.noreply.github.com>
2024-05-14 19:08:55 +0200

    alias: ease git commit

--- config/alias.sh
+++ config/alias.sh
@@ -23,10 +23,10 @@ alias g='git'
 #Added fetch to fetch all changes (new branch, etc.), not only changes from main branch.
 alias gf='git checkout main && git fetch && git pull'
 alias gitalias='git config --get-regexp ^alias'
-alias gr="./gradlew"
-
+alias gac='git add . && git commit -m'
 
 ########################
 ####      Misc      ####
 ########################
+alias gr="./gradlew"
 alias helpscreen="echo 'screen cmd: ctrl + a, then d to detach. screen -r to resume. screen -ls to list.'"
