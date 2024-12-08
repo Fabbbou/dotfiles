fd0aa897b911fa2378a3527269c674ab00b351d8
Fabbbou <18026601+Fabbbou@users.noreply.github.com>
2024-12-08 18:08:48 +0100

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -7,8 +7,3 @@ echo "Installing dotfiles"
 mv dotfiles .dotfiles
 echo -e "\n#Init dotfiles\nsource $HOME/.dotfiles/config/main.sh" >> "$RC_PATH"
 git config --global include.path "$HOME/.dotfiles/config/gitconfig"
-echo "# Add here every changes that are local to your system, and that shouldn't be kept in git repo.
-
-# Example
-# alias ss='ssh user@ip -p port'
-" > "$HOME/.dotfiles/local.sh"
