fa936dc20bb08272a77c9414ef479ef0b8b2b2cf
Fabbbou <18026601+Fabbbou@users.noreply.github.com>
2024-12-08 18:18:08 +0100

    add local.sh in install folder

--- .install.sh
+++ .install.sh
@@ -7,3 +7,10 @@ echo "Installing dotfiles"
 mv dotfiles .dotfiles
 echo -e "\n#Init dotfiles\nsource $HOME/.dotfiles/config/main.sh" >> "$RC_PATH"
 git config --global include.path "$HOME/.dotfiles/config/gitconfig"
+echo "#Add changes local customization that wont be commited here
+#Example:
+#alias ss='ssh user@ip -p port'
+" > "$HOME/.dotfiles/local.sh"
+source "$RC_PATH"
+echo "Dotfiles installed in $HOME/.dotfiles and ready to use!"
+echo "You can now delete the current folder $PWD"
