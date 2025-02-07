bd2d3e5554e7ac47d2276c6c20aebdf362607239
Fabbbou <18026601+Fabbbou@users.noreply.github.com>
2025-02-07 15:53:39 +0100

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -2,7 +2,7 @@ RC_PATH="$HOME/.bashrc"
 
 cd $HOME
 echo "Cloning repo"
-git clone https://github.com/Fabbbou/dotfiles.git
+git clone git@github.com:Fabbbou/dotfiles.git
 echo "Installing dotfiles"
 mv dotfiles .dotfiles
 echo -e "\n#Init dotfiles\nsource $HOME/.dotfiles/config/main.sh" >> "$RC_PATH"
